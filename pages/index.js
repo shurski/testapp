17ae208848a3e7af93e0e5af6f5a0b51d271f3a2
shurski <shurski@scnsoft.com>
2022-05-05 10:38:20 +0300

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+const index = () => {
+    return (
+        <div>
+            <div>
+                 <a href="/">Главная</a>
+                 <a href="/users">Пользователи</a>
+             
+            </div>
+            <h1>Главная страница</h1>
+        </div>
+    );
+};
+
+export default index;
